9df17505cee53106b57fd481b2e1fa019740d03e
DrippyDreDrizzle <apsm99@icloud.com>
2026-07-29 20:46:48 +0100

    Change ElectricTitle text from 'Jordan Rivers' to 'André Marjolin'

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -20,7 +20,7 @@ export default function Intro({ onEnter }) {
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.2, duration: 0.5 }}
         >
-          <ElectricTitle text="Jordan Rivers" />
+          <ElectricTitle text="André Marjolin" />
         </motion.div>
         <motion.p
           className="intro-role"
